aa954c3d8e77fff1982a21a9ba2919a96bd9d63c
Joe Cross <joe.mcross@gmail.com>
2013-11-02 12:50:09 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import io
+import os
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README')
+version = '0.0.1'
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        errcode = tox.cmdline(self.test_args)
+        sys.exit(errcode)
+
+setup(
+    name='pyservice',
+    version=version,
+    url='http://github.com/numberoverzero/pyservice/',
+    license='MIT',
+    author='Joe Cross',
+    tests_require=['pytest', 'tox'],
+    install_requires=[],
+    cmdclass={'test': Tox},
+    author_email='joe.mcross@gmail.com',
+    description='web services with python made easy',
+    long_description=long_description,
+    packages=['pyservice'],
+    include_package_data=True,
+    platforms='any',
+    test_suite='pyservice.test',
+)
---
+++ tasks.py
@@ -0,0 +1,20 @@
+import invoke
+
+def run(*args, **kwargs):
+    kwargs.update(echo=True)
+    return invoke.run(*args, **kwargs)
+
+@invoke.task
+def clean():
+    run("rm -rf .tox/")
+    run("rm -rf dist/")
+    run("rm -rf pyservice.egg-info/")
+
+@invoke.task('clean')
+def build():
+    run("python setup.py develop")
+    pass
+
+@invoke.task('clean', 'build')
+def test():
+    run('tox')
