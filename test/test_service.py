f29ec3a8d0493251a5d15be11c504fe79c6c331c
Joe Cross <joe.mcross@gmail.com>
2013-11-12 23:58:17 -0800

    Split out app testing

--- test/test_service.py
+++ test/test_service.py
@@ -123,19 +123,3 @@ def test_raise_unknown_exception():
 
     with pytest.raises(pyservice.ServerException):
         service.raise_("MyException", "message")
-
-def test_service_routing():
-    data = j('{"name": "ServiceName", "operations": [{"name":"ConcatOperation", "input": ["a", "b"], "output": ["ab"]}]}')
-    service = pyservice.parse_service(data)
-
-    @service.operation("ConcatOperation")
-    def concat(a, b):
-        return a + b
-
-    input = {"a": "Hello", "b": "World"}
-    route = "/ServiceName/ConcatOperation"
-
-    app = webtest.TestApp(service._app)
-    response = app.post_json(route, input)
-
-    assert response.json == {"ab": "HelloWorld"}
