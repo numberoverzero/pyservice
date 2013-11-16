ea06e871ffa5bf3279d1f0a1e588bdfbdcaf000c
Joe Cross <joe.mcross@gmail.com>
2013-11-16 11:03:53 -0800

    Set up Stack/Layer for large layer overhaul
    Layers will have a single handle_request(context, next) method instead
    of trying to invoke handlers for events in the right order.  This also
    makes writing Layers more straightforward, since each just wraps a
    call to next.handle_request(context)

--- pyservice.py
+++ pyservice.py
@@ -361,3 +361,29 @@ class Layer(object):
     def __init__(self, service=None, **kwargs):
         if service:
             service._register_layer(self)
+    def handle_request(self, context, next):
+        # Do some pre-request work
+
+        # Have the next layer process the request
+        next.handle_request(context)
+
+        # Do some post-request work
+
+class Stack(object):
+    def __init__(self, layers=None):
+        self.layers = layers or []
+        self.index = 0
+
+    def append(self, layer):
+        self.layers.append(layer)
+
+    def extend(self, iterable):
+        self.layers.extend(iterable)
+
+    def handle_request(self, context):
+        # End of the chain
+        if self.index >= len(self.layers):
+            return
+        layer = self.layers[self.index]
+        self.index += 1
+        layer.handle_request(context, self)
---
+++ test/test_layers.py
@@ -0,0 +1,8 @@
+import pyservice
+
+
+
+# test two layers called in order
+
+# test layer on_exception called
+
