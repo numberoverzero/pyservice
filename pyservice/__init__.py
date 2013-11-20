9c7acc7e0c4973a85b4e703d2e05312d4d727cd6
Joe Cross <joe.mcross@gmail.com>
2013-11-19 22:44:29 -0800

    Reorder __all__

--- pyservice/__init__.py
+++ pyservice/__init__.py
@@ -1,4 +1,6 @@
 from pyservice.client import Client
+from pyservice.layer import Layer
+from pyservice.operation import Operation
 from pyservice.service import Service
 
-__all__ = ["Client", "Service"]
+__all__ = ["Client", "Layer", "Operation", "Service"]
