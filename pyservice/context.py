173aba72cad0c6c3602b2ae4e1b8bd4e5773bd3b
Joe Cross <joe.mcross@gmail.com>
2014-12-11 00:04:35 -0800

    Add building blocks Container, Context
    These will be used for request/response dicts, as well as
    containers for plugins to pass functions and values to the
    operation function or other plugins.

---
+++ pyservice/context.py
@@ -0,0 +1,28 @@
+"""
+RequestContext stores state relevant to the current request, as well as
+keeping track of the plugin execution order and providing a simple method
+`advance` for calling the next plugin in the chain.
+"""
+import collections
+
+
+class Container(collections.defaultdict):
+    DEFAULT_FACTORY = lambda: None
+
+    def __init__(self):
+        super().__init__(self, Container.DEFAULT_FACTORY)
+
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+
+class RequestContext(object):
+    def __init__(self, service, operation):
+        self.service = service
+        self.operation = operation
+
+    def execute(self):
+        self.service.continue_execution(self)
