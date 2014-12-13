6ab89e9899a08d73358dcac43a8c4aa83264fddf
Joe Cross <joe.mcross@gmail.com>
2014-12-12 20:37:04 -0800

    Restructure project

--- setup.py
+++ setup.py
@@ -1,35 +1,35 @@
-from __future__ import print_function
 from setuptools import setup
-import io
-import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
-
-long_description = read('README')
-version = '0.9.0'
 
 setup(
     name='pyservice',
-    version=version,
-    url='http://github.com/numberoverzero/pyservice/',
-    license='MIT',
-    author='Joe Cross',
-    install_requires=['requests', 'ujson'],
-    author_email='joe.mcross@gmail.com',
+    version='0.9.0',
     description='web services with python made easy',
-    long_description=long_description,
-    packages=['pyservice'],
-    include_package_data=True,
+    long_description=open('README.md').read(),
+    author='Joe Cross',
+    author_email='joe.mcross@gmail.com',
+    url='http://github.com/numberoverzero/pyservice/',
+    py_modules=['pyservice'],
+    scripts=['pyservice.py'],
+    install_requires=['requests', 'ujson'],
+    license='MIT',
     platforms='any',
-    test_suite='pyservice.test',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='wsgi web api framework soa'
 )
