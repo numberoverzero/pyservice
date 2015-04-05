c5a617db987fda0302cf5963bbc41e8d0887347d
Joe Cross <joe.mcross@gmail.com>
2015-04-05 04:28:26 -0700

    Clean up test helper, add example

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,33 @@ import pytest
 
 @pytest.fixture
 def observe(monkeypatch):
-    def patch(module, func):
-        original_func = getattr(module, func)
+    """
+    Wrap a function so its call history can be inspected.
 
-        def wrapper(*args, **kwargs):
-            result = original_func(*args, **kwargs)
-            self.calls[self.last_call] = (args, kwargs, result)
-            self.last_call += 1
+    Example:
+
+    # foo.py
+    def func(bar):
+        return 2 * bar
+
+    # test.py
+    import pytest
+    import foo
+
+    def test_func(observe):
+        observer = observe(foo, "func")
+        assert foo.func(3) == 6
+        assert foo.func(-5) == -10
+        assert len(observer.calls) == 2
+    """
+    class ObserverFactory:
+        def __init__(self, module, func):
+            self.original_func = getattr(module, func)
+            self.calls = []
+            monkeypatch.setattr(module, func, self)
+
+        def __call__(self, *args, **kwargs):
+            result = self.original_func(*args, **kwargs)
+            self.calls.append((args, kwargs, result))
             return result
-
-        self = wrapper
-        self.calls = {}
-        self.last_call = 0
-        monkeypatch.setattr(module, func, wrapper)
-        return wrapper
-    return patch
+    return ObserverFactory
